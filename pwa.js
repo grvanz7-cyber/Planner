af1b7e32a678c9537e0784727243e949582219cb
grvanz7-cyber <grvanz7@gmail.com>
2026-09-07 14:58:46 -0400

    Refresh study set script cache version

--- pwa.js
+++ pwa.js
@@ -10,7 +10,7 @@
     './study-page.js?v=20260912',
     './study-plans.js?v=20260912',
     './study-sessions.js?v=20260912',
-    './study-set-button-fix.js?v=20260912'
+    './study-set-button-fix.js?v=20260916'
   ];
 
   scripts.forEach(src=>{
